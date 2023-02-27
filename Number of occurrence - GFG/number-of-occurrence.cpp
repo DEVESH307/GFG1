a4acdb2d2cb495fd84a61dc753e07d67352f506e
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-02-27 14:23:41 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -7,19 +7,72 @@ using namespace std;
 
 // } Driver Code Ends
 //User function template for C++
+// class Solution{
+// public:	
+// 	/* if x is present in arr[] then returns the count
+// 		of occurrences of x, otherwise returns 0. */
+// 	int count(int arr[], int n, int x) {
+// 	    // code here
+// 	    int ans = 0;
+// 	    for(int i = 0; i < n; i++){
+// 	        if(arr[i] == x){
+// 	            ans++;
+// 	        }
+// 	    }
+// 	    return ans;
+// 	}
+// };
 class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
+	int firstOccurance(int arr[], int n, int x){
+        int first = -1;
+        int low = 0;
+        int high = n-1;
+        int mid;
+        while(low <= high){
+            mid = low+(high-low)/2;
+            if(arr[mid] == x){
+                first = mid;
+                high = mid-1;
+            }
+            else if(arr[mid] < x){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return first;
+    }
+    int lastOccurance(int arr[], int n, int x){
+        int last = -1;
+        int low = 0;
+        int high = n-1;
+        int mid;
+        while(low <= high){
+            mid = low+(high-low)/2;
+            if(arr[mid] == x){
+                last = mid;
+                low = mid+1;
+            }
+            else if(arr[mid] < x){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return last;   
+    }
 	int count(int arr[], int n, int x) {
 	    // code here
-	    int ans = 0;
-	    for(int i = 0; i < n; i++){
-	        if(arr[i] == x){
-	            ans++;
-	        }
-	    }
-	    return ans;
+	    int _first = firstOccurance(arr, n, x);
+        int _last = lastOccurance(arr, n, x);
+        
+        if(_first == -1 || _last == -1) return 0;
+        else return (_last - _first + 1);
 	}
 };
 
