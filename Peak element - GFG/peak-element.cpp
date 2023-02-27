236338afa6e93c83a8e281ce81534e9aef179ba5
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2023-02-27 16:08:36 +0530

    Added solution - LeetHub

--- Peak element - GFG/peak-element.cpp
+++ Peak element - GFG/peak-element.cpp
@@ -9,6 +9,22 @@ using namespace std;
 
 // arr: input array
 // n: size of array
+// class Solution{
+//     public:
+//     int peakElement(int arr[], int n){
+//       // Your code here
+//         if(n == 1) return 0;
+//         if(arr[0] >= arr[1]) return 0;
+//         if(arr[n-1] >= arr[n-2]) return n-1;
+//         for(int i = 1; i < n-1; i++){
+//           if(arr[i] >= arr[i-1] && arr[i] >= arr[i+1]){
+//               return i;
+//           }
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution{
     public:
     int peakElement(int arr[], int n){
@@ -16,14 +32,26 @@ class Solution{
         if(n == 1) return 0;
         if(arr[0] >= arr[1]) return 0;
         if(arr[n-1] >= arr[n-2]) return n-1;
-        for(int i = 1; i < n-1; i++){
-           if(arr[i] >= arr[i-1] && arr[i] >= arr[i+1]){
-               return i;
-           }
+        
+        int low = 0;
+        int high = n-1;
+        int mid;
+        while(low <= high){
+            mid = low+(high-low)/2;
+            if(arr[mid] >= arr[mid-1] && arr[mid] >= arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid] <= arr[mid+1]){
+                low = mid+1;
+            }
+            else{
+                high = mid;
+            }
         }
         return -1;
     }
 };
+
 
 //{ Driver Code Starts.
 
